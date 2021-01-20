e1268133b81d1f59ced5871fd922ef91a15ddf7a
willian-carrato <thecastieel07@gmail.com>
2021-01-19 21:11:30 -0300

    criando header- estrutura e logo

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { HeaderArea } from './styled';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-      <HeaderArea>...</HeaderArea>
+      <HeaderArea>
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <img src="logo1.png" alt="logo" height="40" width="100" />
+            </Link>
+          </div>
+        </div>
+      </HeaderArea>
     </div>
   );
 }
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-  height: 100px;
-  background-color: #ccc;
+  background-color: #fff;
+  height: 60px;
+  border-bottom: 1px solid #ccc;
+  .container {
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+  }
+  .logo {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    height: 60px;
+  }
 `;
